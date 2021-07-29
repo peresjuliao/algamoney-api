507e8bdf63c87a130291e3cc57a7abc0568984d5
Peres Julião <peresjuliao@gmail.com>
2021-07-29 10:31:02 -0300

    second commit

---
+++ src/main/java/com/peresjuliao/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.peresjuliao.algamoneyapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.peresjuliao.algamoneyapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
